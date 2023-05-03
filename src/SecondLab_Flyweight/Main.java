00a41c47d52ba6826df61669f0b064e1c4e40cb0
panjuksergy <panjuksergy@gmail.com>
2023-05-04 00:35:15 +0300

    Uncommented main method of second LAB

--- src/SecondLab_Flyweight/Main.java
+++ src/SecondLab_Flyweight/Main.java
@@ -1,33 +1,33 @@
-//package SecondLab_Flyweight;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        DocFactory docFactory = new DocFactory();
-//
-//        List<IDoc> docs = new ArrayList<>();
-//
-//
-//        for (int i = 0; i< 3; i++){
-//            docs.add(docFactory.getDocBySpecialty("Anesthesiologist"));
-//        }
-//        for (int i = 0; i< 3; i++){
-//            docs.add(docFactory.getDocBySpecialty("Nurse"));
-//        }
-//        for (int i = 0; i< 3; i++){
-//            docs.add(docFactory.getDocBySpecialty("Oncologist"));
-//        }
-//        for (int i = 0; i< 3; i++){
-//            docs.add(docFactory.getDocBySpecialty("Surgeon"));
-//        }
-//
-//
-//        for(IDoc doc: docs){
-//            doc.HealPeople();
-//        }
-//
-//    }
-//
-//}
+package SecondLab_Flyweight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        DocFactory docFactory = new DocFactory();
+
+        List<IDoc> docs = new ArrayList<>();
+
+
+        for (int i = 0; i< 3; i++){
+            docs.add(docFactory.getDocBySpecialty("Anesthesiologist"));
+        }
+        for (int i = 0; i< 3; i++){
+            docs.add(docFactory.getDocBySpecialty("Nurse"));
+        }
+        for (int i = 0; i< 3; i++){
+            docs.add(docFactory.getDocBySpecialty("Oncologist"));
+        }
+        for (int i = 0; i< 3; i++){
+            docs.add(docFactory.getDocBySpecialty("Surgeon"));
+        }
+
+
+        for(IDoc doc: docs){
+            doc.HealPeople();
+        }
+
+    }
+
+}
